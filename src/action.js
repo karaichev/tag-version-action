76d989d3fec31e58b260d1a79c71313cb2a7fdbd
Sergey Karaichev <karaichev@gmail.com>
2021-10-13 15:37:11 +0300

    test

--- src/action.js
+++ src/action.js
@@ -6,11 +6,6 @@ async function run() {
   const GITHUB_TOKEN = core.getInput('GITHUB_TOKEN');
   const octokit = github.getOctokit(GITHUB_TOKEN);
 
-  console.log({
-    ...github.context.owner,
-    ...github.context.repo,
-  });
-
   const tags = await octokit.rest.repos.listTags({
     ...github.context.owner,
     ...github.context.repo,
