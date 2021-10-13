6a1f62fb9278b427bb99db35a25d68b88211b2e5
Sergey Karaichev <karaichev@gmail.com>
2021-10-13 13:19:31 +0300

    Tag list

--- src/action.js
+++ src/action.js
@@ -1,5 +1,16 @@
+const core = require('@actions/core')
+const github = require('@actions/github');
+const { context } = require('@actions/github/lib/utils');
+
 async function run() {
-    console.log('Hello');
+  const GITHUB_TOKEN = core.getInput('GITHUB_TOKEN');
+  const octokit = github.getOctokit(GITHUB_TOKEN);
+
+  const tags = await octokit.repos.listTags({
+    ...github.context.repo,
+  });
+  
+  console.log(tags);
 }
 
 run();
